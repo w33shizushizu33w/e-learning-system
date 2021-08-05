030de52e121eac90081415066d44ad02517c4f95
w33shizushizu33w <shizue_1707@yahoo.co.jp>
2021-08-05 18:47:26 +0900

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/" => "static_pages#home"
   get "/about" => "static_pages#about"
   get "/contact" => "static_pages#contact"
+  get "/signup" => "users#new"
 end
