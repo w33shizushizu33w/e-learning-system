d2d683243218d5077f21c87c8109847cb262a619
w33shizushizu33w <shizue_1707@yahoo.co.jp>
2021-08-10 15:33:31 +0900

    made change

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,16 @@ User.create!(name: "SHIZUE",
     email: "email@email.com",
     password: "123abc456def",
     password_confirmation: "123abc456def",
-    is_admin: 1)
+    is_admin: true)
 
 50.times do |n|
     name = Faker::Name.name
     email = "username#{n+1}@email.org"
     password = "123abc456def"
-    is_admin = 0
     User.create!(name: name,
                 email: email,
                 password: password,
                 password_confirmation: password,
-                is_admin: is_admin)
+                is_admin: false)
 
 end   
