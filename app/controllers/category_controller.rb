b76745380342d2a8139a689fb13f8a45a0f5a942
w33shizushizu33w <shizue_1707@yahoo.co.jp>
2021-08-11 18:21:19 +0900

    made change

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -7,10 +7,26 @@ class CategoryController < ApplicationController
     @category = Category.find(params[:id])
   end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:info] = "Saved Successfully"
+      redirect_to create_path
+    else
+      flash[:danger] = "Creating failed"
+      render "create"
+    end
+  end
+
   def index
     @category = Category.all
   end
 
   def edit
   end
+
+  private
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end 
 end
