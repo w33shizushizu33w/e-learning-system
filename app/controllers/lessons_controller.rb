6ad948d34b5d959a71ec1ad5f12f09a8aea87c07
w33shizushizu33w <shizue_1707@yahoo.co.jp>
2021-08-16 15:19:17 +0900

    made changes

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,5 @@
 class LessonsController < ApplicationController
   def index
-    @category = Category.paginate(page: params[:page], per_page: 10 )
+    @category = Category.paginate(page: params[:page], per_page: 6 )
   end
 end
